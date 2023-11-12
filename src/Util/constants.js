2cbebb320796fe1ef85a3a27f96f5c3d01682f85
yeokyoung song <spolice0324@gmail.com>
2023-11-12 13:32:27 +0900

    feat : 필요한 상수 추가
    - Input, Output, Error 메세지 정의

---
+++ src/Util/constants.js
@@ -0,0 +1,12 @@
+const INPUT_MESSAGE = Object.freeze({
+  VISIT_DATE:
+    '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해주세요!)\n',
+  ORDER:
+    '주문하실 메뉴와 메뉴 개수를 알려주세요. (예시 : 해산물파스타-2, 초코케이크-1\n',
+});
+
+const OUTPUT_MESSAGE = Object.freeze({});
+
+const ERROR_MESSAGE = Object.freeze({});
+
+export { INPUT_MESSAGE, OUTPUT_MESSAGE, ERROR_MESSAGE };
