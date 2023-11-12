8493b2828632856c2d4c415d6ff4c7ab6053951c
yeokyoung song <spolice0324@gmail.com>
2023-11-12 14:22:12 +0900

    feat : 사용자 입력에 따른 출력 문구 표시 OutputView 구현
    - 값 출력 전 타이틀 출력 메소드 구현

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,9 +1,34 @@
+import { Console } from '@woowacourse/mission-utils';
+import { OUTPUT_MESSAGE } from '../Util/constants';
+
 const OutputView = {
-  printMenu() {
-    Console.print('<주문 메뉴>');
-    // ...
+  printIntro() {
+    Console.print(OUTPUT_MESSAGE.INTRO);
   },
-  // ...
+  printMenuTitle() {
+    Console.print(OUTPUT_MESSAGE.ORDER_MENU);
+  },
+  printBeforeDiscount() {
+    Console.print(OUTPUT_MESSAGE.BEFORE_DISCOUNT);
+  },
+  printAfterDiscount() {
+    Console.print(OUTPUT_MESSAGE.AFTER_DISCOUNT);
+  },
+  printGift() {
+    Console.print(OUTPUT_MESSAGE.GIFT);
+  },
+  printBenefits() {
+    Console.print(OUTPUT_MESSAGE.BENEFITS);
+  },
+  printTotalBenefitAmount() {
+    Console.print(OUTPUT_MESSAGE.TOTAL_BENEFIT_AMOUNT);
+  },
+  printEventBadge() {
+    Console.print(OUTPUT_MESSAGE.EVENT_BADGE);
+  },
+  printPreview(date) {
+    Console.print(OUTPUT_MESSAGE.PREVIEW(date));
+  },
 };
 
 export default OutputView;
