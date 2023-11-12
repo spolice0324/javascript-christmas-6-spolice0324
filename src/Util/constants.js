450faffc26e508f88768f32e76e244a61817fbc8
yeokyoung song <spolice0324@gmail.com>
2023-11-12 14:20:54 +0900

    feat : 필요한 상수 추가
    - Input, Output에 필요한 메세지 정의
    - 메뉴와 출력 타이틀 정의

--- src/Util/constants.js
+++ src/Util/constants.js
@@ -2,11 +2,63 @@ const INPUT_MESSAGE = Object.freeze({
   VISIT_DATE:
     '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해주세요!)\n',
   ORDER:
-    '주문하실 메뉴와 메뉴 개수를 알려주세요. (예시 : 해산물파스타-2, 초코케이크-1\n',
+    '주문하실 메뉴와 메뉴 개수를 알려주세요. (예시 : 해산물파스타-2, 초코케이크-1)\n',
 });
 
-const OUTPUT_MESSAGE = Object.freeze({});
+const OUTPUT_MESSAGE = Object.freeze({
+  INTRO: '안녕하세요! 우테코 식당 12월 이벤트 플래너 입니다.\n',
+  PREVIEW: (date) => `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
+  ORDER_MENU: '<주문 메뉴>\n',
+  BEFORE_DISCOUNT: '<할인 전 총주문 금액>\n',
+  AFTER_DISCOUNT: '<할인 후 예상 결제 금액>\n',
+  GIFT: '<증정 메뉴>\n',
+  BENEFITS: '<혜택 내역>\n',
+  TOTAL_BENEFIT_AMOUNT: '<총혜택 금액>\n',
+  EVENT_BADGE: '<12월 이벤트 배지>\n',
+});
+
+const WORD = Object.freeze({
+  WON: '원',
+  NONE: '없음',
+  BADGE_STAR: '별',
+  BADGE_TREE: '트리',
+  BADGE_SANTA: '산타',
+});
+
+const MENU_KIND = Object.freeze({
+  APPETIZER: '애피타이저',
+  MAIN: '메인',
+  DESSERT: '디저트',
+  DRINK: '음료',
+});
+
+const MENU_ITEMS = Object.freeze({
+  SOUP: { name: '양송이 수프', price: 6000 },
+  TAPAS: { name: '타파스', price: 5500 },
+  SALAD: { name: '시저샐러드', price: 8000 },
+  STEAK: { name: '티본스테이크', price: 55000 },
+  BARBEQUE: { name: '바비큐립', price: 55000 },
+  SEAFOOD_PASTA: { name: '해산물파스타', price: 35000 },
+  CHRISTMAS_PASTA: { name: '크리스마스 파스타', price: 25000 },
+  DESSERT: { name: '초코케이크', price: 15000 },
+  ICECREAM: { name: '아이스크림', price: 5000 },
+  COKE: { name: '제로콜라', price: 3000 },
+  WINE: { name: '레드와인', price: 60000 },
+  CHAMPAGNE: { name: '샴페인', price: 25000 },
+});
+
+const MENU = Object.freeze({
+  [MENU_KIND.APPETIZER]: [MENU_ITEMS.SOUP, MENU_ITEMS.TAPAS, MENU_ITEMS.SALAD],
+  [MENU_KIND.MAIN]: [
+    MENU_ITEMS.STEAK,
+    MENU_ITEMS.BARBEQUE,
+    MENU_ITEMS.SEAFOOD_PASTA,
+    MENU_ITEMS.CHRISTMAS_PASTA,
+  ],
+  [MENU_KIND.DESSERT]: [MENU_ITEMS.DESSERT, MENU_ITEMS.ICECREAM],
+  [MENU_KIND.DRINK]: [MENU_ITEMS.COKE, MENU_ITEMS.WINE, MENU_ITEMS.CHAMPAGNE],
+});
 
 const ERROR_MESSAGE = Object.freeze({});
 
-export { INPUT_MESSAGE, OUTPUT_MESSAGE, ERROR_MESSAGE };
+export { INPUT_MESSAGE, OUTPUT_MESSAGE, WORD, ERROR_MESSAGE, MENU };
