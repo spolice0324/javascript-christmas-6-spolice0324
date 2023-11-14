f9e4d2d27ac83b278be3a8fe6191403a1cbd6f72
yeokyoung song <spolice0324@gmail.com>
2023-11-14 19:06:20 +0900

    feat : 고객의 입력과 출력을 처리하는 App 클래스 구현
    - InputView에서 입력값을 받아오는 로직으로 구현
    - OuputView를 통해 출력되는 부분 로직 분리하여 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Client from './Domain/Client';
+import InputView from './View/InputView';
+import OutputView from './View/OutputView';
+
 class App {
-  async run() {}
+  async run() {
+    const client = new Client(
+      await InputView.readDate(),
+      await InputView.readOrder(),
+    );
+    const date = client.getDate();
+    this.printOutput(client, date);
+  }
+
+  printOutput(client, date) {
+    OutputView.printIntro();
+    OutputView.printPreview(date);
+    OutputView.printMenuTitle(client);
+    OutputView.printBeforeDiscount(client);
+    OutputView.printGift(client);
+    OutputView.printBenefit(client);
+    OutputView.printDiscountAmount(client);
+    OutputView.printAfterDiscount(client);
+    OutputView.printEventBadge(client);
+  }
 }
 
 export default App;
