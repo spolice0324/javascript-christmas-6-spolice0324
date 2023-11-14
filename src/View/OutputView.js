2eb1213ca21692832f0e3251c426879c0d11b745
yeokyoung song <spolice0324@gmail.com>
2023-11-14 19:04:03 +0900

    feat : OuputView에서 타이틀 아래 출력하는 부분 추가
    - 출력해야하는 내역 모두 추가

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,33 +1,56 @@
 import { Console } from '@woowacourse/mission-utils';
-import { OUTPUT_MESSAGE } from '../Util/constants';
+import { INFO, OUTPUT_MESSAGE } from '../Util/constants';
 
 const OutputView = {
   printIntro() {
-    Console.print(OUTPUT_MESSAGE.INTRO);
+    Console.print(`${OUTPUT_MESSAGE.INTRO}\n`);
   },
-  printMenuTitle() {
-    Console.print(OUTPUT_MESSAGE.ORDER_MENU);
-  },
-  printBeforeDiscount() {
-    Console.print(OUTPUT_MESSAGE.BEFORE_DISCOUNT);
-  },
-  printAfterDiscount() {
-    Console.print(OUTPUT_MESSAGE.AFTER_DISCOUNT);
-  },
-  printGift() {
-    Console.print(OUTPUT_MESSAGE.GIFT);
-  },
-  printBenefits() {
-    Console.print(OUTPUT_MESSAGE.BENEFITS);
-  },
-  printTotalBenefitAmount() {
-    Console.print(OUTPUT_MESSAGE.TOTAL_BENEFIT_AMOUNT);
-  },
-  printEventBadge() {
-    Console.print(OUTPUT_MESSAGE.EVENT_BADGE);
-  },
+
   printPreview(date) {
-    Console.print(OUTPUT_MESSAGE.PREVIEW(date));
+    Console.print(`${OUTPUT_MESSAGE.TITLE.PREVIEW(date)}`);
+  },
+
+  printMenuTitle(client) {
+    Console.print(
+      `${OUTPUT_MESSAGE.TITLE.ORDER_MENU}\n${client.getOrderList()}\n`,
+    );
+  },
+  printBeforeDiscount(client) {
+    const output = `${OUTPUT_MESSAGE.TITLE.BEFORE_DISCOUNT}\n${client
+      .getBeforeDiscount()
+      .toLocaleString()}${INFO.UNIT}\n`;
+    Console.print(output);
+  },
+
+  printGift(client) {
+    const output = `${OUTPUT_MESSAGE.TITLE.GIFT}\n${client.getGift()}\n`;
+    Console.print(output);
+  },
+
+  printBenefit(client) {
+    const output = `${
+      OUTPUT_MESSAGE.TITLE.BENEFIT
+    }\n${client.getBenefitList()}\n`;
+    Console.print(output);
+  },
+
+  printDiscountAmount(client) {
+    const output = `${OUTPUT_MESSAGE.TITLE.BENEFIT_AMOUNT}\n${client
+      .getDiscountAmount()
+      .toLocaleString()}${INFO.UNIT}\n`;
+    Console.print(output);
+  },
+
+  printAfterDiscount(client) {
+    const output = `${OUTPUT_MESSAGE.TITLE.AFTER_DISCOUNT}\n${client
+      .getAfterDiscount()
+      .toLocaleString()}${INFO.UNIT}\n`;
+    Console.print(output);
+  },
+
+  printEventBadge(client) {
+    const output = `${OUTPUT_MESSAGE.TITLE.BADGE}\n${client.getEventBadge()}\n`;
+    Console.print(output);
   },
 };
 
