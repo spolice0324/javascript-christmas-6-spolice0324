3f228af288ffda168e63a5a5787160d99799bf19
yeokyoung song <spolice0324@gmail.com>
2023-11-15 01:00:34 +0900

    test : Discount 클래스 테스트 구현
    - 평일 할인 메소드 테스트

--- __tests__/DiscountTest.js
+++ __tests__/DiscountTest.js
@@ -1,5 +1,5 @@
 import Discount from '../src/Domain/Discount';
-import { DATE } from '../src/Util/constants';
+import { DATE, DISCOUNT } from '../src/Util/constants';
 
 describe('Discount 클래스 로직 테스트', () => {
   let discount;
@@ -7,7 +7,7 @@ describe('Discount 클래스 로직 테스트', () => {
     discount = new Discount();
   });
 
-  describe('isPeriod 메서드 테스트', () => {
+  describe('크리스마스 기간 이벤트 할인 메소드 테스트', () => {
     test('크리스마스 이벤트 기간 내에 있는 경우 할인이 올바르게 계산되어야 한다.', () => {
       const date = DATE.EVENT_START + 2;
       const result = discount.isPeriod(date);
@@ -20,6 +20,30 @@ describe('Discount 클래스 로직 테스트', () => {
       const date = DATE.EVENT_END + 1;
       const result = discount.isPeriod(date);
       expect(result).toBe(0);
+    });
+  });
+
+  describe('평일 할인 메소드 테스트', () => {
+    test('평일(일~토)이라면 주문 내역 중 디저트 메뉴를 할인하는 금액이 올바르게 계산되어야 한다. - 디저트 메뉴가 0개일 때', () => {
+      const date = DATE.EVENT_START + 3;
+      const menu = { 레드와인: 1, 해산물파스타: 1 };
+      const result = discount.isWeekday(date, menu);
+      const expectedDiscount = 0;
+      expect(expectedDiscount).toBe(0);
+    });
+    test('평일(일~토)이라면 주문 내역 중 디저트 메뉴를 할인하는 금액이 올바르게 계산되어야 한다. - 디저트 메뉴가 1개일 때', () => {
+      const date = DATE.EVENT_START + 3;
+      const menu = { 초코케이크: 1, 레드와인: 1, 해산물파스타: 1 };
+      const result = discount.isWeekday(date, menu);
+      const expectedDiscount = -2023;
+      expect(result).toBe(expectedDiscount);
+    });
+    test('평일(일~토)이라면 주문 내역 중 디저트 메뉴를 할인하는 금액이 올바르게 계산되어야 한다. - 디저트 메뉴가 여러개일 때', () => {
+      const date = DATE.EVENT_START + 3;
+      const menu = { 초코케이크: 4, 레드와인: 1, 해산물파스타: 1 };
+      const result = discount.isWeekday(date, menu);
+      const expectedDiscount = -2023 * 4;
+      expect(result).toBe(expectedDiscount);
     });
   });
 });
