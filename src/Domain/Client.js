c1fb2166fe3233b8711363951a93409ed6d0600b
yeokyoung song <spolice0324@gmail.com>
2023-11-14 18:23:45 +0900

    feat : Client 클래스 구현
    - 날짜와 주문을 필드로가진 고객에 대한 클래스 구현
    - 혜택내역을 계산하는 메소드 추가

---
+++ src/Domain/Client.js
@@ -0,0 +1,22 @@
+import Discount from './Discount';
+
+class Client {
+  #date;
+  #order;
+
+  constructor(date, order) {
+    this.#date = date;
+    this.#order = order;
+  }
+
+  getDate() {
+    return this.#date;
+  }
+
+  calculateBenefits() {
+    const discount = new Discount();
+    return discount.calculateDiscount(this.#date, this.#order);
+  }
+}
+
+export default Client;
