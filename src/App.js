83734ec820fbd8107b7e7495325af4121a57a6a0
yeokyoung song <spolice0324@gmail.com>
2023-11-15 00:09:34 +0900

    refactor : App 클래스 내 로직을 PromotionController로 분리
    - App 에서 PromotionController를 호출하는 방식으로 수정
    - PromotionController에서 입출력을 담당하도록 로직 적용

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import Client from './Domain/Client';
-import InputView from './View/InputView';
-import OutputView from './View/OutputView';
+import PromotionController from './Domain/PromotionController';
 
 class App {
-  async run() {
-    const client = new Client(
-      await InputView.readDate(),
-      await InputView.readOrder(),
-    );
-    const date = client.getDate();
-    this.printOutput(client, date);
+  constructor() {
+    this.promotionController = new PromotionController();
   }
 
-  printOutput(client, date) {
-    OutputView.printIntro();
-    OutputView.printPreview(date);
-    OutputView.printMenuTitle(client);
-    OutputView.printBeforeDiscount(client);
-    OutputView.printGift(client);
-    OutputView.printBenefit(client);
-    OutputView.printDiscountAmount(client);
-    OutputView.printAfterDiscount(client);
-    OutputView.printEventBadge(client);
+  async run() {
+    await this.promotionController.insertInput();
   }
 }
 
