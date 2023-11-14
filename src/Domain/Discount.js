ba928ab390a7e05cd29225f3d3e61e1f29964534
yeokyoung song <spolice0324@gmail.com>
2023-11-14 17:16:55 +0900

    feat : 평일 기간에 대한 할인 계산 메소드 구현
    - 평일(일~목)인지 확인하고, 디저트 메뉴 1개당  할인 적용

--- src/Domain/Discount.js
+++ src/Domain/Discount.js
@@ -1,13 +1,25 @@
-import { DATE } from '../Util/constants';
+import { DATE, INFO, MENU, MENU_KIND } from '../Util/constants';
 
 class Discount {
   checkPeriod(date) {
     let periodDiscount = 0;
-    if (date >= DATE.EVNET_START && date <= DATE.EVENT_END) {
+    if (date >= DATE.EVENT_START && date <= DATE.EVENT_END) {
       periodDiscount +=
         -DATE.PERIOD_DISCOUNT + (date - 1) * -DATE.PER_DAY_DISCOUNT;
     }
     return periodDiscount;
+  }
+
+  checkWeek(date, menu) {
+    let weekDisCount = 0;
+    if (!(date % 7 === 1 || date % 7 === 2)) {
+      let count = 0;
+      Object.keys(menu).forEach(name => {
+        if (MENU[name].kind === MENU_KIND.DESSERT) count += menu[name];
+      });
+      weekDisCount += -INFO.WEEK_DISCOUNT * count;
+    }
+    return weekDisCount;
   }
 }
 
