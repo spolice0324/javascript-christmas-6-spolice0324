0885ae4d5c4fe261ab246431df4f3eb43dbcb05e
yeokyoung song <spolice0324@gmail.com>
2023-11-14 18:25:12 +0900

    feat : 주문 메뉴 반환하는 메소드 추가
    - 주문된 메뉴 목록을 반환하는 getMenuList 메소드 추가

--- src/Domain/Client.js
+++ src/Domain/Client.js
@@ -17,6 +17,14 @@ class Client {
     const discount = new Discount();
     return discount.calculateDiscount(this.#date, this.#order);
   }
+
+  getMenuList() {
+    let output = '';
+    Object.entries(this.#order).forEach(([name, count]) => {
+      output += `${name} ${count}${INFO.COUNT}\n`;
+    });
+    return output;
+  }
 }
 
 export default Client;
